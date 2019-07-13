aa0ef54c4b02648078ad6427906c35a4d005d42c
fl28 <franciscolessa28@outlook.com>
2019-07-13 10:03:21 -0300

    Inserindo rota raiz

--- recipies/config/routes.rb
+++ recipies/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Root para abrir nesta página
+  root 'recipies#index' 
   resources :recipies
 end
